4b09479d64453f160eaa10279da664929801b9d1
Denis Vygovskiy <dshaded@gmail.com>
2017-05-26 14:08:15 +1000

    Better skel/main.c example

--- skel/src/main.c
+++ skel/src/main.c
@@ -1,6 +1,104 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <util/delay.h>
+#include <stdbool.h>
+
+#include "lasertag.h"
 #include "dsh-common.h"
 
 
-int main() {
+/************************************ MACROS ************************************/
+#define CONST_1 123              // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#define CONST_2 (CONST_1 >> 1)   // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
+
+/************************************ DATA ************************************/
+static const uint8_t LASERTAG_CMD_1[] = CMD_SIMPLE(LT_NEW_GAME); // XXXXXXXXXXXXXXXXXXXXX
+static const uint8_t LASERTAG_CMD_2[] = CMD_SIMPLE(LT_KILL);     // XXXXXXXXXXXXXXXXXXXXX
+static const uint8_t* const LASERTAG_COMMANDS[] = {              // XXXXXXXXXXXXXXXXXXXXX
+	LASERTAG_CMD_1, LASERTAG_CMD_2                               // XXXXXXXXXXXXXXXXXXXXX
+};                                                               // XXXXXXXXXXXXXXXXXXXXX
+
+
+/************************************ VARS ************************************/
+static volatile uint8_t moduleVar;                               // XXXXXXXXXXXXXXXXXXXXX
+volatile bool globalVar;                                         // XXXXXXXXXXXXXXXXXXXXX
+
+
+/************************************ FORWARD DECLARATIONS ************************************/
+static void hwInit();
+
+static void waitInterrupt();                                                // XXXXXXXXXX
+static uint16_t intFunction(uint8_t param1);                                // XXXXXXXXXX
+static void lasertagCmdFunction(const uint8_t* cmdPointer, uint8_t param);  // XXXXXXXXXX
+
+
+/************************************ INTERRUPTS ************************************/
+EMPTY_INTERRUPT(INT0_vect)               //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+ISR(TIMER1_COMPA_vect, ISR_NAKED) {      //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+	//naked interrupt                    //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+	PORTB ^= _BV(PB0);                   //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+	reti();                              //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+}                                        //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+ISR(ADC_vect) {                          //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+	static uint16_t staticLocalVar = 0;  //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+	uint8_t localVar = ADCH;             //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+	staticLocalVar += localVar;          //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+	moduleVar += localVar;               //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+}                                        //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+/************************************ MAIN CODE ************************************/
+int main() {
+	hwInit();
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+	static uint16_t staticLocalVar = 0;
+	uint8_t localVar = 3;
+	bool boolVar = true;
+
+	while(true) {
+		waitInterrupt();
+		staticLocalVar = intFunction(localVar);
+		boolVar = staticLocalVar == 888;
+		if(boolVar) {
+			lasertagCmdFunction(LASERTAG_COMMANDS[0], 1);
+			_delay_ms(2000);
+		}
+	}
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+}
+
+uint16_t intFunction(uint8_t param1) {  //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+	return ADC * param1;                //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+}                                       //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+void lasertagCmdFunction(const uint8_t* cmdPointer, uint8_t param) {  //XXXXXXXXXXXXXXXXX
+	moduleVar = cmdPointer[param];                                    //XXXXXXXXXXXXXXXXX
+}                                                                     //XXXXXXXXXXXXXXXXX
+
+void waitInterrupt() {
+	sleep_enable();
+	sei();
+	sleep_cpu();
+	sleep_disable();
+	cli();
+}
+
+/************************************ HWINIT ************************************/
+void hwInit() {
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+	OCR1A  = 123;
+	OCR1B  = 0;
+	TCCR1A = _BV(COM1B1) | _BV(COM1B0) | _BV(WGM10) | _BV(WGM11);
+	TCCR1B = _BV(CS10) | _BV(WGM12) | _BV(WGM13);
+
+	DDRB = _BV(DDB0) | _BV(DDB1) | _BV(DDB2);
+
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+	sei();
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 }
