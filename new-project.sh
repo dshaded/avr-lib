1a581af14715f620705d8cbcb0b3e6037d95ae98
Denis Vygovskiy <dshaded@gmail.com>
2019-04-03 19:45:21 +1000

    Simplified new-project

--- new-project.sh
+++ new-project.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-if [ "$(ls -A)" ] ; then
-	echo "Directory must be empty"
-else
-	git init
-	git submodule add git@github.com:dshaded/avr-lib.git lib
-	git submodule init
-	cp -r lib/skel/* lib/skel/.gitignore .
-	git add .
-	git commit -m 'Initial commit'
-fi
+git submodule add git@github.com:dshaded/avr-lib.git lib
+git submodule init
+cp -r lib/skel/* lib/skel/.gitignore .
